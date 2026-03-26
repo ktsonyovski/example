127040462752776e01bda8751dbfb4d85712c1e0
ktsonyovski <ktsonyovski@outlook.com>
2026-03-26 23:34:01 +0200

    Add initial .gitignore, main.py, and requirements.txt files

---
+++ app/main.py
@@ -0,0 +1,8 @@
+"""Main source code for FastAPI user management"""
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="User Management API",
+    description="User management API for examples",
+    summary="User management API for creating and deleting users in a SQLite database"
+)
