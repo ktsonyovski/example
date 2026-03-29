66898974b5ad7b7d27e1e49fb8e8173b7088e142
ktsonyovski <ktsonyovski@outlook.com>
2026-03-29 23:30:10 +0300

    Add user authentication and management endpoints

--- app/main.py
+++ app/main.py
@@ -11,3 +11,19 @@ app = FastAPI(
 def root():
     """Root function"""
     return "Hello World"
+
+@app.post("/auth/login")
+def login(username: str, password: str):
+    """Simple login"""
+
+@app.get("/users")
+def users_list():
+    """Show users list"""
+
+@app.post("/users")
+def create_user(username: str, password: str):
+    """Create user"""
+
+@app.delete(f"/users{id}")
+def delete_user(id: int):
+    """Delete user"""
