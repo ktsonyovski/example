4a4cfb2e2e1c4c877653d208fc83124840992efd
ktsonyovski <ktsonyovski@outlook.com>
2026-04-12 23:51:42 +0300

    Add create_table function and improve user management queries in db.py

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Table, Column, MetaData, String
+from sqlalchemy import create_engine, Table, Column, MetaData, String, text
 
 engine = create_engine('sqlite:///database.db', echo=True)
 meta = MetaData()
@@ -11,12 +11,19 @@ users_table = Table(
     Column("password", String, nullable=False)
 )
 
+def create_table():
+    connection.execute(text("CREATE TABLE IF NOT EXISTS users (username string, password string)"))
+
 def create_user(username: str, password: str) -> None:
-    add_user = users_table.insert().values(username=username, password=password)
-    connection.execute(add_user)
+    add_user_query = users_table.insert().values(username=username, password=password)
+    connection.execute(add_user_query)
     connection.commit()
 
 def delete_user(username: str) -> None:
-    del_user = users_table.delete().where(users_table.c.username == username)
-    connection.execute(del_user)
+    del_user_query = users_table.delete().where(users_table.c.username == username)
+    connection.execute(del_user_query)
     connection.commit()
+
+def get_all_users():
+    get_users_query = connection.execute(text("SELECT * FROM users")) 
+    return [dict(row._mapping) for row in get_users_query]
