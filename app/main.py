065b912bd387f57c37d6dad4aaf2cb054540c8f0
ktsonyovski <ktsonyovski@outlook.com>
2026-04-12 23:51:54 +0300

    Add lifecycle management for FastAPI app and implement user retrieval endpoint

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@
 from fastapi import FastAPI
 
 from app.schemas import CreateUser
-from app.db import create_user, delete_user
+from app.db import create_user, delete_user, create_table, get_all_users
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifecycle(app:FastAPI):
+    create_table()
+    yield
 
 app = FastAPI(
     title="User Management API",
     description="User management API for examples",
-    summary="User management API for creating and deleting users in a SQLite database"
+    summary="User management API for creating and deleting users in a SQLite database",
+    lifespan=lifecycle
 )
 
 @app.post(f"/api/create_user")
@@ -19,3 +26,8 @@ def post_user(user: CreateUser):
 def del_user(username: str):
     """Delete user"""
     delete_user(username=username)
+
+@app.get(f"/api/users")
+def show_users():
+    """Show all users"""
+    return get_all_users()
