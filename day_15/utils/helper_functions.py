ea59d887297b8f98f7ea8a596e8f6358177a289a
ktsonyovski <ktsonyovski@outlook.com>
2026-05-09 20:33:40 +0300

    Refactor resource loading functions and update JSON structure for availability and costs

--- day_15/utils/helper_functions.py
+++ day_15/utils/helper_functions.py
@@ -2,9 +2,35 @@
 import json
 
 RESOURCES_PATH = r"/home/ktsonyovski/projects/example/day_15/resources"
+AVAIL_RESOURCES = "avail_res"
+COST_FILE = "cost"
 
-def load_resources(resource_path: str = RESOURCES_PATH):
-    """Load resources"""
-    with open(f"{resource_path}/avail_res.json", "r", encoding="utf-8") as resources:
+def load_json(file: str, resource_path: str = RESOURCES_PATH) -> dict:
+    """Load JSON data from a file"""
+    with open(f"{resource_path}/{file}.json", "r", encoding="utf-8") as resources:
         data = json.load(resources)
-        return data["resources"]
+        return data
+
+def get_avail_res(file: str = AVAIL_RESOURCES) -> dict:
+    """Get resources from avail_resources file"""
+    resource_file = load_json(file)
+    avail_resources = resource_file["resources"]
+    return avail_resources
+
+def get_drink_res(drink: str, file: str = COST_FILE) -> dict:
+    """Get resources from avail_resources file"""
+    resource_file = load_json(file)
+    drink_res = resource_file[drink]
+    return drink_res
+
+def check_sufficient_resources(drink: str) -> bool:
+    """Check if there are sufficient resources to make the drink"""
+    drink_resources = dict(get_drink_res(drink))
+    avail_resources = dict(get_avail_res())
+    
+    for resource, needed in drink_resources.items():
+        available = avail_resources.get(resource, 0)
+        if needed > available:
+            print(f"Not enough {resource}! Got {available}, needed {needed}")
+            return False
+    return True
